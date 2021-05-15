8ba7b3ecff79e6170b2a466fcef2e8fa04d2b5a0
Ravishankar Swami <shivswamiv@gmail.com>
2021-05-15 21:11:34 +0530

    Update commits on site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Vionsys IT Solutions India PVT LTD';
+  title = 'Vionsys IT Solutions India';
 
   subject='Register for free course on AWS Solution Architect.'
+
+
+  submit()
+  {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {  MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
